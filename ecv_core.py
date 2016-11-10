5a3ce23db43a44919f21752cbce508f24a6e356a
frbaroni <fernando.rbaroni@gmail.com>
2016-11-10 16:05:06 -0200

    Basic image drawing on browser

--- ecv_core.py
+++ ecv_core.py
@@ -1,5 +1,6 @@
-import cv2
+import time
 import threading
+import cv2
 
 class ErgoCVBase:
     """
@@ -22,7 +23,7 @@ class ErgoCVBase:
             matched against.
         """
         pass
-    def getPosition(self):
+    def getCurrentPosition(self):
         """
             Return the last detected face position.
         """
@@ -47,8 +48,8 @@ class ErgoCVBase:
         pass
 
 class ErgoCV(ErgoCVBase):
-    def __init__(self):
-        self.webcam = cv2.VideoCapture(0)
+    def __init__(self, camera_index):
+        self.webcam = cv2.VideoCapture(camera_index)
         self.hasWindows = False
         self.ergoPosition = None
         self.currentPosition = None
@@ -63,7 +64,7 @@ class ErgoCV(ErgoCVBase):
     def setErgoPosition(self, ergoPosition):
         self.ergoPosition = ergoPosition
 
-    def getErgoPosition(self, ergoPosition):
+    def getErgoPosition(self):
         return self.ergoPosition
 
     def getCurrentPosition(self):
@@ -137,55 +138,71 @@ class ErgoCV(ErgoCVBase):
                 ))
             self.show('Debug, [a] to adjust, [q] to exit', self.img)
 
-class ErgoCVThread(threading.Thread):
-    def __init__(self, ergoCV, tick_delay):
-        threading.Thread.__init__(self)
-        self.tick_delay = tick_delay
-        self.terminated = False
-        self.ergoCV = ergoCV
-
-    def run(self):
-        while not self.terminated:
-            ergoCV.update()
-            print('{0} ergo: {1} current: {2}'.format(
-                "GOOD" if ergoCV.good_position() else "BAD",
-                ergoCV.getErgoPosition(),
-                ergoCV.getCurrentPosition()
-                ))
-            time.sleep(self.tick_delay)
-
 class ErgoCVThreaded(ErgoCVBase):
-    def __init__(self, camera_index, tick_delay):
-        self.ergoCV = ErgoCV(camera_index)
-        self.lock = threading.RLock()
-        self.thread = ErgoCVThread(ergoCV, tick_delay)
-    
-    def lockedScope(self, fn):
-        self.lock.acquire()
-        fn()
-        self.lock.release()
+    class Container(threading.Thread):
+        def __init__(self, ergoCV, tick_delay):
+            self.lock = threading.RLock()
+            threading.Thread.__init__(self)
+            self.tick_delay = tick_delay
+            self.terminated = False
+            self.ergoCV = ergoCV
 
-    def start(self):
+        def run(self):
+            while not self.terminated:
+                self.lock.acquire()
+                self.ergoCV.update()
+                print('{0} ergo: {1} current: {2}'.format(
+                    "GOOD" if self.ergoCV.good_position() else "BAD",
+                    self.ergoCV.getErgoPosition(),
+                    self.ergoCV.getCurrentPosition()
+                    ))
+                self.lock.release()
+                time.sleep(self.tick_delay)
+
+        def setTickDelay(self, tick_delay):
+            self.tick_delay = tick_delay
+
+    def __init__(self):
+        self.ergoCV = None
+        self.thread = None
+    
+    def lockedDo(self, fn):
+        if self.thread:
+            self.thread.lock.acquire()
+            fn()
+            self.thread.lock.release()
+
+    def lockedCall(self, fn):
+        if self.thread:
+            self.thread.lock.acquire()
+            res = fn()
+            self.thread.lock.release()
+            return res
+        else:
+            return None
+
+    def start(self, camera_index, tick_delay):
+        self.ergoCV = ErgoCV(camera_index)
+        self.thread = ErgoCVThreaded.Container(self.ergoCV, tick_delay)
         self.thread.start()
 
     def setErgoPosition(self, ergoPosition):
-        self.lockedScope(lambda: self.ergoCV.setErgoPosition(ergoPosition))
-        pass
+        self.lockedDo(lambda: self.ergoCV.setErgoPosition(ergoPosition))
 
     def getErgoPosition(self):
-        pass
+        return self.lockedCall(lambda: self.ergoCV.getErgoPosition())
 
-    def getPosition(self):
-        pass
+    def getCurrentPosition(self):
+        return self.lockedCall(lambda: self.ergoCV.getCurrentPosition())
 
     def setTickDelay(self, tick_delay):
-        self.thread.tick_delay = tick_delay
+        self.lockedDo(lambda: self.thread.setTickDelay(tick_delay))
 
     def getImage(self, toExtension):
-        pass
+        return self.lockedCall(lambda: self.ergoCV.getImage(toExtension))
 
     def good_position(self):
-        pass
+        return self.lockedCall(lambda: self.ergoCV.good_position())
 
     def update(self):
-        pass
+        self.lockedDo(lambda: self.ergoCV.update())
--- ecv_gui.py
+++ ecv_gui.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+import datetime
 import flask
-from ecv_core import ErgoCV
+from ecv_core import ErgoCVThreaded
+
+camera_index = 0
+tick_delay = 0.100
 
 app = flask.Flask(__name__)
+ergoCV = ErgoCVThreaded()
 
 @app.route('/')
 def index():
@@ -10,8 +15,15 @@ def index():
 
 @app.route('/image', methods=['GET'])
 def image():
-    return ''
+    result, img = ergoCV.getImage('.jpg')
+    response = flask.Response(img.tobytes(), mimetype="image/jpg")
+    response.headers['Last-Modified'] = datetime.datetime.now()
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
 
 def run_gui(port, debug):
     app.config['DEBUG'] = debug
+    ergoCV.start(camera_index, tick_delay)
     app.run(port=port)
