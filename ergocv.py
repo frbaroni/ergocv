48d6ab4aec507b4b4fef0a350975684b1ef05669
Fernando Rodrigues Baroni <fernando@baroni.tech>
2017-08-03 13:23:59 -0300

    Removed Flask application, migrating to wxWidgets

--- ergocv.py
+++ ergocv.py
@@ -2,32 +2,22 @@
 import sys
 import getopt
 from ecv_core import ErgoCV
-from ecv_gui import run_gui
-
-DEFAULT_PORT = 35275  # Flask in Phone Keypad
 
 
 def usage():
     print("""Usage: python3 ergocv.py [options]
     Options:
-        -p port, --port port
-        change the port number, default to {0}
+        -h, --help
+        this help
 
-        -f, --flask-debug
-        enable flask debug
-
-        -e camera_index, --ecv-debug camera_index
+        -e camera_index, --ecv camera_index
         enable ErgoCV debug
-    """.format(DEFAULT_PORT))
+    """)
 
 
 def main(argv):
-    debug = False
-    port = DEFAULT_PORT
-
     try:
-        opts, args = getopt.getopt(argv, "hfe:p:", ["help", "flask-debug",
-                                                    "ecv-debug=", "port="])
+        opts, args = getopt.getopt(argv, "he:", ["help", "ecv="])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -36,18 +26,13 @@ def main(argv):
         if opt in ("-h", "--help"):
             usage()
             sys.exit()
-        elif opt in ("-f", "--flask-debug"):
-            print("Flask debug enabled")
-            debug = True
-        elif opt in ("-e", "--ecv-debug"):
+        elif opt in ("-e", "--ecv"):
             print("ErgoCV debug enabled")
             ergoCv = ErgoCV()
             ergoCv.setCameraIndex(int(arg))
             ergoCv.run_debug()
             sys.exit(0)
-        elif opt in ("-p", "--port"):
-            port = arg
-    run_gui(port, debug)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
